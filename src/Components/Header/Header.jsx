5fcc210284140b879aa2262eef9ec0a4c7e3c289
anikdas400 <anikchandra400@gmail.com>
2023-09-14 03:09:47 +0600

    load data form json

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>React Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
